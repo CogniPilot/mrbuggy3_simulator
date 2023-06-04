6c67d65d81afe80aa926d47c1ff3d1dec88291a7
Benjamin Perseghetti <bperseghetti@rudislabs.com>
2023-06-04 13:48:22 -0400

    Update models and launch.
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>

--- mrbuggy3_gz_bringup/launch/gz_nav2.launch.py
+++ mrbuggy3_gz_bringup/launch/gz_nav2.launch.py
@@ -1,15 +1,12 @@
-import os
-from pathlib import Path
-
+from os import environ
 from ament_index_python.packages import get_package_share_directory
 
-from launch import LaunchContext, LaunchDescription, SomeSubstitutionsType, Substitution
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
-from launch.conditions import IfCondition, LaunchConfigurationEquals
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-
+from launch.actions import ExecuteProcess
+from launch.conditions import LaunchConfigurationEquals, IfCondition
 from launch_ros.actions import Node
 
 
@@ -31,71 +28,28 @@ ARGUMENTS = [
                           description='Run corti'),
     DeclareLaunchArgument('cerebri', default_value='true',
                           choices=['true', 'false'],
-                          description='Run corti'),
-    DeclareLaunchArgument('use_sim_time', default_value='true',
-                          choices=['true', 'false'],
-                          description='use_sim_time'),
+                          description='Run cerebri'),
     DeclareLaunchArgument('world', default_value='depot',
                           description='GZ World'),
-    DeclareLaunchArgument('model', default_value='lidar',
-                          choices=['base', 'lidar'],
-                          description='MR Buggy3 Model'),
-    DeclareLaunchArgument('robot_name', default_value='mrbuggy3',
-                          description='Robot name')
+    DeclareLaunchArgument('debugger', default_value='false',
+                          choices=['true', 'false'],
+                          description='Run cerebri with gdb debugger.'),
+    DeclareLaunchArgument('uart_shell', default_value='false',
+                          choices=['true', 'false'],
+                          description='Run cerebri with UART shell.'),
+    DeclareLaunchArgument('spawn_model', default_value='true',
+                          choices=['true', 'false'],
+                          description='Spawn El Mandadero Model'),
+    DeclareLaunchArgument('use_sim_time', default_value='true',
+                          choices=['true', 'false'],
+                          description='Use sim time'),
 ]
 
 
 def generate_launch_description():
 
-    # Directories
-    pkg_mrbuggy3_gz_bringup = get_package_share_directory(
-        'mrbuggy3_gz_bringup')
-    pkg_mrbuggy3_gz_resource = get_package_share_directory(
-        'mrbuggy3_gz_resource')
-    pkg_mrbuggy3_description = get_package_share_directory(
-        'mrbuggy3_description')
-    pkg_mrbuggy3_nav2 = get_package_share_directory(
-        'mrbuggy3_nav2')
-    pkg_mrbuggy3_rviz = get_package_share_directory(
-        'mrbuggy3_rviz')
-    pkg_corti = get_package_share_directory('corti')
-    pkg_cerebri = get_package_share_directory('cerebri')
-    pkg_ros_gz_sim = get_package_share_directory(
-        'ros_gz_sim')
-
-    # Paths
-    gz_sim_launch = PathJoinSubstitution(
-        [pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])
-    mrbuggy3_ros_gz_bridge_launch = PathJoinSubstitution(
-        [pkg_mrbuggy3_gz_bringup, 'launch', 'ros_gz_bridge.launch.py'])
-    rviz_launch = PathJoinSubstitution(
-        [pkg_mrbuggy3_rviz, 'launch', 'view_robot.launch.py'])
-    nav2_launch = PathJoinSubstitution(
-        [pkg_mrbuggy3_nav2, 'launch', 'nav2.launch.py'])
-    corti_launch = PathJoinSubstitution(
-        [pkg_corti, 'launch', 'corti.launch.py'])
-    cerebri_launch = PathJoinSubstitution(
-        [pkg_cerebri, 'launch', 'cerebri.launch.py'])
-    slam_launch = PathJoinSubstitution(
-        [pkg_mrbuggy3_nav2, 'launch', 'slam.launch.py'])
-    localization_launch = PathJoinSubstitution(
-        [pkg_mrbuggy3_nav2, 'launch', 'localization.launch.py'])
-    robot_description_launch = PathJoinSubstitution(
-        [pkg_mrbuggy3_description, 'launch', 'robot_description.launch.py'])
-
 
     # Parameters
-    param_file_cmd = DeclareLaunchArgument(
-        'param_file',
-        default_value=PathJoinSubstitution(
-            [pkg_mrbuggy3_gz_bringup, 'config', 'mrbuggy3_node.yaml']),
-        description='MR Buggy3 param file')
-
-    declare_map_yaml_cmd = DeclareLaunchArgument(
-        'map',
-        default_value=PathJoinSubstitution(
-            [pkg_mrbuggy3_nav2, 'maps', 'depot.yaml']),
-        description='Full path to map yaml file to load')
 
     declare_x = DeclareLaunchArgument(
         'x',
@@ -117,133 +71,213 @@ def generate_launch_description():
         default_value=['0'],
         description='yaw position')
 
+    declare_map_yaml = DeclareLaunchArgument(
+        'map_yaml', 
+        default_value=[LaunchConfiguration('world'), '.yaml'],
+        description='Map yaml'),
+
 
     # Launch configurations
     x = LaunchConfiguration('x')
     y = LaunchConfiguration('y')
     z = LaunchConfiguration('z')
     yaw = LaunchConfiguration('yaw')
-    mrbuggy3_node_yaml_file = LaunchConfiguration('param_file')
 
-    # Gazebo
-    gz_sim = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([gz_sim_launch]),
-        launch_arguments=[
-            ('gz_args', [
-                LaunchConfiguration('world'), '.sdf',
-                ' -v 1',
-                ' -r ',
-                ' --gui-config ', PathJoinSubstitution(
-                    [pkg_mrbuggy3_gz_bringup,
-                     'gui',
-                     LaunchConfiguration('model'),
-                     'gui.config'])])
-        ]
+    synapse_ros = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+            [get_package_share_directory('synapse_ros'), 'launch', 'synapse_ros.launch.py'])]),
+        launch_arguments=[('host', ['192.0.2.1']),
+                          ('port', '4242')]
     )
+
+    synapse_gz = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+            [get_package_share_directory('synapse_gz'), 'launch', 'synapse_gz.launch.py'])]),
+        launch_arguments=[('host', ['127.0.0.1']),
+                          ('port', '4241')],
+    )
+
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+            [get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'])]),
+        launch_arguments=[('gz_args', [LaunchConfiguration('world'), '.sdf', ' -v 1', ' -r'])]
+    )
+
+    cerebri = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+            [get_package_share_directory('cerebri_bringup'), 'launch', 'cerebri.launch.py'])]),
+        launch_arguments=[('debugger', LaunchConfiguration('debugger')),
+                          ('vehicle', 'mrbuggy3'),
+                          ('uart_shell', LaunchConfiguration('uart_shell'))],
+    )
+
+    joy = Node(
+        namespace='cerebri/in',
+        package='joy',
+        executable='joy_node',
+        output='screen'
+    )
+
+    clock_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        #namespace='cerebri',
+        name='clock_bridge',
+        output='screen',
+        arguments=[
+            '/clock' + '@rosgraph_msgs/msg/Clock' + '[gz.msgs.Clock'
+        ])
+
+    lidar_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        #namespace='cerebri',
+        name='lidar_bridge',
+        output='screen',
+        parameters=[{
+            'use_sim_time': LaunchConfiguration('use_sim_time')
+        }],
+        arguments=[
+            ['/world/default/model/mrbuggy3/link/lidar_link/sensor/lidar/scan' +
+             '@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan']
+        ],
+        remappings=[
+            ('/world/default/model/mrbuggy3/link/lidar_link/sensor/lidar/scan',
+             '/scan')
+        ])
+
+    odom_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        #namespace='cerebri',
+        name='odom_bridge',
+        output='screen',
+        parameters=[{
+            'use_sim_time': LaunchConfiguration('use_sim_time')
+            }],
+        arguments=[
+            '/model/mrbuggy3/odometry@nav_msgs/msg/Odometry[gz.msgs.Odometry'
+            ],
+        remappings=[
+            ('/model/mrbuggy3/odometry', '/cerebri/in/odometry')
+            ])
+
+    odom_base_tf_bridge = Node(
+        package='ros_gz_bridge', 
+        executable='parameter_bridge',
+        #namespace='cerebri',
+        name='odom_base_tf_bridge',
+        output='screen',
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}],
+        arguments=[
+           ['/model/mrbuggy3/pose' +
+            '@tf2_msgs/msg/TFMessage' +
+            '[gz.msgs.Pose_V']
+        ],
+        remappings=[
+           (['/model/mrbuggy3/pose'], '/tf')
+        ])
+
+    pose_bridge = Node(
+        package='ros_gz_bridge', 
+        executable='parameter_bridge',
+        #namespace='cerebri',
+        name='pose_bridge',
+        output='screen',
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}],
+        arguments=[
+           ['/model/mrbuggy3/pose' +
+            '@tf2_msgs/msg/TFMessage' +
+            '[gz.msgs.Pose_V']
+        ],
+        remappings=[
+           (['/model/mrbuggy3/pose'],
+            '/_internal/sim_ground_truth_pose')
+        ])
+
 
     # Robot description
     robot_description = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([robot_description_launch]),
-        launch_arguments=[('model', LaunchConfiguration('model')),
-                          ('use_sim_time', LaunchConfiguration('use_sim_time'))]
-    )
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+        [get_package_share_directory('mrbuggy3_description'), 'launch', 'robot_description.launch.py'])]),
+        launch_arguments=[('use_sim_time', LaunchConfiguration('use_sim_time'))])
 
-    # Spawn MR Buggy3
     spawn_robot = Node(
         package='ros_gz_sim',
         executable='create',
+        name='spawn_robot',
         arguments=[
-            '-world', LaunchConfiguration('world'),
-            '-name', LaunchConfiguration('robot_name'),
+            '-world', 'default',
+            '-name', 'mrbuggy3',
             '-x', x,
             '-y', y,
             '-z', z,
             '-Y', yaw,
-            '-file', PathJoinSubstitution(
-              [pkg_mrbuggy3_gz_resource,
-              'models',
-              LaunchConfiguration('robot_name'),
-              'model.sdf'])
+            '-file', PathJoinSubstitution([get_package_share_directory(
+                'mrbuggy3_gz_resource'),
+                'models/mrbuggy3/model.sdf'])
         ],
-        output='screen')
+        output='screen',
+        condition=IfCondition(LaunchConfiguration("spawn_model")))
 
-    # ROS GZ bridge
-    mrbuggy3_ros_gz_bridge = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([mrbuggy3_ros_gz_bridge_launch]),
-        launch_arguments=[
-            ('model', LaunchConfiguration('model')),
-            ('use_sim_time', LaunchConfiguration('use_sim_time'))
-        ]
-    )
-
-    # Rviz2
     rviz2 = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([rviz_launch]),
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+        [get_package_share_directory(
+        'mrbuggy3_rviz'), 'launch', 'view_robot.launch.py'])]),
         condition=IfCondition(LaunchConfiguration('rviz')),
-        launch_arguments=[
-            ('model', LaunchConfiguration('model')),
-            ('use_sim_time', LaunchConfiguration('use_sim_time'))
-        ]
-    )
+        launch_arguments=[('use_sim_time', LaunchConfiguration('use_sim_time'))])
 
-    # NAV2
     nav2 = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([nav2_launch]),
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+        [get_package_share_directory(
+        'mrbuggy3_nav2'), 'launch', 'nav2.launch.py'])]),
         condition=IfCondition(LaunchConfiguration('nav2')),
-        launch_arguments=[
-            ('use_sim_time', LaunchConfiguration('use_sim_time'))
-        ]
-    )
+        launch_arguments=[('use_sim_time', LaunchConfiguration('use_sim_time'))])
 
-    # Cerebri
-    cerebri = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([cerebri_launch]),
-    )
-
-    # Corti
     corti = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([corti_launch]),
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+        [get_package_share_directory('corti'), 'launch', 'corti.launch.py'])]),
         condition=IfCondition(LaunchConfiguration('corti')),
-        launch_arguments=[
-            ('use_sim_time', LaunchConfiguration('use_sim_time'))
-        ]
-    )
+        launch_arguments=[('use_sim_time', LaunchConfiguration('use_sim_time'))])
 
     slam = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([slam_launch]),
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+        [get_package_share_directory(
+        'mrbuggy3_nav2'), 'launch', 'slam.launch.py'])]),
         condition=LaunchConfigurationEquals('localization', 'slam'),
-        launch_arguments=[
-            ('sync', LaunchConfiguration('sync')),
-            ('use_sim_time', LaunchConfiguration('use_sim_time'))
-        ]
-    )
+        launch_arguments=[('use_sim_time', LaunchConfiguration('use_sim_time')),
+            ('sync', LaunchConfiguration('sync'))])
 
     localization = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([localization_launch]),
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+        [get_package_share_directory(
+        'mrbuggy3_nav2'), 'launch', 'localization.launch.py'])]),
         condition=LaunchConfigurationEquals('localization', 'localization'),
-        launch_arguments=[
-            ('map', LaunchConfiguration('map')),
-            ('use_sim_time', LaunchConfiguration('use_sim_time'))
-        ]
-    )
+        launch_arguments=[('use_sim_time', LaunchConfiguration('use_sim_time')),
+            ('map', PathJoinSubstitution([get_package_share_directory(
+                'mrbuggy3_nav2'), 'maps', LaunchConfiguration('map_yaml')]))])
 
     # Define LaunchDescription variable
-    ld = LaunchDescription(ARGUMENTS)
-    ld.add_action(param_file_cmd)
-    ld.add_action(declare_map_yaml_cmd)
-    ld.add_action(declare_x)
-    ld.add_action(declare_y)
-    ld.add_action(declare_z)
-    ld.add_action(declare_yaw)
-    ld.add_action(gz_sim)
-    ld.add_action(mrbuggy3_ros_gz_bridge)
-    ld.add_action(rviz2)
-    ld.add_action(robot_description)
-    ld.add_action(spawn_robot)
-    ld.add_action(nav2)
-    ld.add_action(cerebri)
-    ld.add_action(corti)
-    ld.add_action(slam)
-    ld.add_action(localization)
-    return ld
-
+    return LaunchDescription(ARGUMENTS + [
+        robot_description,
+        declare_x,
+        declare_y,
+        declare_z,
+        declare_yaw,
+        synapse_ros,
+        synapse_gz,
+        gz_sim,
+        cerebri,
+        joy,
+        odom_bridge,
+        clock_bridge,
+        lidar_bridge,
+        odom_base_tf_bridge,
+        pose_bridge,
+        rviz2,
+        spawn_robot,
+        nav2,
+        corti,
+        slam,
+        localization
+    ])
