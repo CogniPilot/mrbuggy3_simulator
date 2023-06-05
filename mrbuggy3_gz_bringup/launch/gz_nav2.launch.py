9863ac68ee5218056539912ff995bd9af2118a9c
James Goppert <james.goppert@gmail.com>
2023-06-04 20:04:48 -0400

    Add tf_to_odom.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- mrbuggy3_gz_bringup/launch/gz_nav2.launch.py
+++ mrbuggy3_gz_bringup/launch/gz_nav2.launch.py
@@ -159,7 +159,7 @@ def generate_launch_description():
             '/model/mrbuggy3/odometry@nav_msgs/msg/Odometry[gz.msgs.Odometry'
             ],
         remappings=[
-            ('/model/mrbuggy3/odometry', '/cerebri/in/odometry')
+            ('/model/mrbuggy3/odometry', '/odom')
             ])
 
     odom_base_tf_bridge = Node(
@@ -257,6 +257,18 @@ def generate_launch_description():
             ('map', PathJoinSubstitution([get_package_share_directory(
                 'mrbuggy3_nav2'), 'maps', LaunchConfiguration('map_yaml')]))])
 
+    tf_to_odom = Node(
+        package='corti',
+        executable='tf_to_odom',
+        output='screen',
+        parameters=[{
+            'base_frame': 'map',
+            'target_frame': 'base_link',
+            }],
+        remappings=[
+            ('/odom', '/cerebri/in/odometry')
+            ])
+
     # Define LaunchDescription variable
     return LaunchDescription(ARGUMENTS + [
         robot_description,
@@ -279,5 +291,6 @@ def generate_launch_description():
         nav2,
         corti,
         slam,
-        localization
+        localization,
+        tf_to_odom
     ])
